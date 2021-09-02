bc6b0355e78d99e1f77d086ccf4eb879f621a12a
Vibhav Rathee <vibhavrathee77@gmail.com>
2021-09-02 18:55:23 +0530

    first react app

---
+++ src/components/SearchForm.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useGlobalContext } from '../context'
+
+const SearchForm = () => {
+  const {setSearchTerm}= useGlobalContext();
+  const searchValue = React.useRef('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+  const searchCocktail=()=>{
+    setSearchTerm(searchValue.current.value);
+  }
+  React.useEffect(() => {
+    searchValue.current.focus();
+  })
+  return (
+    <section className="section search">
+      <form className="search-form" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label id="name">search your favourite cocktail</label>
+          <input type="text" id="name" ref={searchValue} onChange={searchCocktail}/>
+        </div>
+      </form>
+    </section>
+  )
+}
+
+export default SearchForm
---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">
+        About us
+      </h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique reprehenderit dicta molestias earum at consequatur suscipit vero amet, expedita quae, excepturi ipsum nam natus sint! Explicabo temporibus et ducimus iste?</p>
+    </section>
+  )
+}
+
+export default About
